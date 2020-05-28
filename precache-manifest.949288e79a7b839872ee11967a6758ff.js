dc191834d6b9f048f7133953e1c30c53ce90ad5a
Oxa <oxakost@gmail.com>
2020-05-28 23:51:03 +0300

    Updates

---
+++ precache-manifest.949288e79a7b839872ee11967a6758ff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc73adfd6ec7e493357373451d18750",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "14106b84f8475e051d4e",
+    "url": "/my-app/static/css/main.537442db.chunk.css"
+  },
+  {
+    "revision": "db22ef6d25825da78a98",
+    "url": "/my-app/static/js/2.837bdf0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.837bdf0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14106b84f8475e051d4e",
+    "url": "/my-app/static/js/main.0c41a0c3.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  },
+  {
+    "revision": "eb0652679291f384edb2b415ceae980d",
+    "url": "/my-app/static/media/Peony.eb065267.jpg"
+  },
+  {
+    "revision": "adf3f16bee2eb2672ecd8cf3c1315595",
+    "url": "/my-app/static/media/ava.adf3f16b.png"
+  },
+  {
+    "revision": "adf3f16bee2eb2672ecd8cf3c1315595",
+    "url": "/my-app/static/media/avaMessage.adf3f16b.png"
+  },
+  {
+    "revision": "adf3f16bee2eb2672ecd8cf3c1315595",
+    "url": "/my-app/static/media/avatar.adf3f16b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f50ceefd2bce0c31f011626496023827",
+    "url": "/my-app/static/media/logo.f50ceefd.png"
+  },
+  {
+    "revision": "93a57c9f7d6386c36b6805255eac0d81",
+    "url": "/my-app/static/media/user.93a57c9f.png"
+  }
+]);
